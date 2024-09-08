f12d103029ae434134e3b6f3e11c11e92af5266c
Saarthakkj <saarthak22421@iiitd.ac.in>
2024-09-08 23:16:42 +0530

    vtis config js file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/))  return null
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },react()],
   server: {
     proxy: {
       '/api': {
@@ -16,4 +28,12 @@ export default defineConfig({
     rollupOptions: {
     }
   }
+  ,optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
