d4fb80ec049a45ea28680891721f46c993939ceb
Saarthakkj <saarthak22421@iiitd.ac.in>
2024-09-08 22:57:04 +0530

    main jsx error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -17,4 +16,4 @@ export default defineConfig({
     rollupOptions: {
     }
   }
-})
+});
