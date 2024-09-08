883e20ab432d6b11d7b91eb0701f8e98618d19d1
Saarthakkj <saarthak22421@iiitd.ac.in>
2024-09-08 23:27:41 +0530

    fixed package.json js/jsx file support~

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,12 @@ export default defineConfig({
     {name: 'treat-js-files-as-jsx',
       async transform(code, id) {
         if (!id.match(/src\/.*\.js$/))  return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
         return transformWithEsbuild(code, id, {
           loader: 'jsx',
           jsx: 'automatic',
         })
       },
     },react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://script.google.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
   build: {
     rollupOptions: {
     }
